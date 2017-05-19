59ef503cbd410e7bb11946e287fa6f0bb84c4cdd
Garlandos <amberkgarland@gmail.com>
2017-05-19 19:47:16 +0100

    Step 4: ScoreMaster
    ScoreMaster script now takes lists and returns both the cumulative score & the score frames for each bowl where applicable. Also created ScoreMaster test script to ensure functionality.

---
+++ EarlyListNonsenseScores.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class EarlyListNonsenseScores : MonoBehaviour {
+//    public Text stframe;
+//    // Use this for initialization;
+//    void Start( )
+//    {
+//        stframe = GetComponentInChildren<Text>( );
+//    }
+
+//    // Update is called once per frame
+//    void Update () {
+
+//    }
+//    public void Scores(List<int> pins)
+//    {
+//            if (pins.Count % 2 == 0)
+//        {
+//            int frameBowls = pins.ElementAt(pins.Count-1) + pins.ElementAt(pins.Count - 2);
+//            if (frameBowls == 10)
+//            {
+//                stframe.text = frameBowls.ToString( );
+//            }
+//            else
+//            {
+//                stframe.text = pins.ToString( );
+//            }
+//        }
+//        else
+//        {
+//            stframe.text = pins.ToString( );
+//        }
+//    }
+}
--- ScoreMaster.cs
+++ ScoreMaster.cs
@@ -1,16 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
 
-public class ScoreMaster {
+public class ScoreMaster
+{
+    public static List<int> ScoreCumulative(List<int> rolls)
+    {
+        List<int> cumulativeScores = new List<int>( );
+        int runningTotal = 0;
 
-    public static List<int> ScoreFrames (List<int> rolls)
+        foreach (int frameScore in ScoreFrames(rolls))
+        {
+            if (cumulativeScores.Count <= 9)
+            {
+                runningTotal += frameScore;
+                cumulativeScores.Add(runningTotal);
+            }
+
+        }
+
+        return cumulativeScores;
+    }
+
+
+    public static List<int> ScoreFrames(List<int> rolls)
     {
         List<int> frameList = new List<int>( );
 
-        // Code here
+        for (int i = 1; i < rolls.Count; i += 2)
+        {
+            if (frameList.Count == 10)
+            {
+                break;
+            }
 
+            if (rolls[i - 1] + rolls[i] < 10) // If neither strike nor spare in frame
+            {
+                frameList.Add(rolls[i - 1] + rolls[i]);
+            }
+
+            if (rolls.Count - i <= 1)
+            {
+                break;
+            }
+
+            if (rolls[i - 1] == 10) // If strike in first frame BONUS
+            {
+                i--;
+                frameList.Add(10 + rolls[i + 1] + rolls[i + 2]);
+            }
+            else if (rolls[i - 1] + rolls[i] == 10) // If spare
+            {
+                frameList.Add(10 + rolls[i + 1]);
+            }
+        }
         return frameList;
     }
-
 }
