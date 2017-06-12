7c56d26e55ec3fe7216709fe7de73c97a3886c74
Garlandos <amberkgarland@gmail.com>
2017-06-12 17:20:08 +0100

    ScoreDisplay Tests
    New ScoreDisplay tests added.

--- ScoreDisplay.cs
+++ ScoreDisplay.cs
@@ -2,33 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Text;
 using System.Linq;
 
 public class ScoreDisplay : MonoBehaviour {
     public Text runningTotal;
     public Text[] bowlsText,frameText;
+
 	// Use this for initialization;
 	void Start () {
         runningTotal.text = "0";
         bowlsText[0].text = "X";
 	}
-	
-	// Update is called once per frame;
-	void Update () {
-		
-	}
-    public void FillRollCard(List<int> rolls)
-    {
-        rolls[1] = -1;
-    }
 
-    public void Scores(List<int> scores) // Add framescores to the array
+    public void FillRolls(List<int> rolls)
     {
-        
+        string scoreString = FormatRolls(rolls);
+        for (int i = 0; i < scoreString.Length; i++)
+        {
+            bowlsText[i].text = scoreString[i].ToString( );
+        }
     }
 
     public void TotalScore(List<int> scores)
     {
         runningTotal.text = scores.ElementAt(scores.Count - 1).ToString( );
+    }
+
+    public void FillFrames(List<int> frames)
+    {
+        for (int i = 0; i < frames.Count; i++)
+        {
+            frameText[i].text = frames[i].ToString( );
+        }
+    }
+
+    public static string FormatRolls(List<int> rolls){
+        string output = "";
+        var rollList = new List<string>( );
+
+        for (int i = 0; i < rolls.Count; i++)
+        {
+            if (rolls[i] == 10) //if this first bowl is a 10 display X
+            {
+                rollList.Add("X");
+                if (i % 2 == 0)
+                {
+                    rollList.Add(" ");
+                }
+            }
+            else if (rolls[i] == 0){ //display "-" in place of a "0"
+                rollList.Add("-");
+            } else
+            {
+                if (i%2==1 && (rolls[i - 1] + rolls[i] == 10))
+                {
+                    rollList.Add("/");
+                }
+                else
+                {
+                    rollList.Add(rolls[i].ToString( ));
+                }
+            }
+        }
+        output = string.Join("",rollList.ToArray());
+
+        return output;
     }
 }
---
+++ ScoreDisplayTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using System.Linq;
+
+[TestFixture]
+public class ScoreDisplayTest
+{
+    [Test]
+    public void PassingTest( )
+    {
+        Assert.AreEqual(1, 1);
+    }
+
+    [Test]
+    public void T01Bowl1( )
+    {
+        int[] rolls = { 1 };
+        string rollsString = "1";
+        Assert.AreEqual(rollsString, ScoreDisplay.FormatRolls(rolls.ToList( )));
+    }
+
+    [Test]
+    public void T02Bowl1and2( )
+    {
+        int[] rolls = { 1, 2 };
+        string rollsString = "12";
+        Assert.AreEqual(rollsString, ScoreDisplay.FormatRolls(rolls.ToList( )));
+    }
+
+    [Test]
+    public void T03Bowl10( )
+    {
+        int[] rolls = { 10 };
+        string rollsString = "X ";
+        Assert.AreEqual(rollsString, ScoreDisplay.FormatRolls(rolls.ToList( )));
+    }
+
+    [Test]
+    public void T04Bowl3and0( )
+    {
+        int[] rolls = { 3,0 };
+        string rollsString = "3-";
+        Assert.AreEqual(rollsString, ScoreDisplay.FormatRolls(rolls.ToList( )));
+    }
+
+    [Test]
+    public void T05Bowl10and4( )
+    {
+        int[] rolls = { 10 , 4};
+        string rollsString = "X 4";
+        Assert.AreEqual(rollsString, ScoreDisplay.FormatRolls(rolls.ToList( )));
+    }
+
+    [Test]
+    public void T06Bowl1and9( )
+    {
+        int[] rolls = { 1, 9 };
+        string rollsString = "1/";
+        Assert.AreEqual(rollsString, ScoreDisplay.FormatRolls(rolls.ToList( )));
+    }
+}
