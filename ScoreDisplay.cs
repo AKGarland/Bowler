7ccdac46771d524e6dd30f2d707ab9e9e45179f2
Garlandos <amberkgarland@gmail.com>
2017-06-13 15:24:06 +0100

    Clean Up

--- ScoreDisplay.cs
+++ ScoreDisplay.cs
@@ -9,18 +9,12 @@ public class ScoreDisplay : MonoBehaviour {
     public Text runningTotal;
     public Text[] bowlsText,frameText;
 
-	// Use this for initialization;
-	void Start () {
-        runningTotal.text = "0";
-        bowlsText[0].text = "X";
-	}
-
     public void FillRolls(List<int> rolls)
     {
         string scoreString = FormatRolls(rolls);
-        for (int i = 0; i < scoreString.Length; i++)
+        for (int i = 0; i < scoreString.Length; i++)  
         {
-            bowlsText[i].text = scoreString[i].ToString( );
+            bowlsText[i].text = scoreString[i].ToString( );  // Iterating through each Text box on the bowls part of the scoring frames, this fills in the first box with the first score and so on.
         }
     }
 
@@ -33,7 +27,7 @@ public class ScoreDisplay : MonoBehaviour {
     {
         for (int i = 0; i < frames.Count; i++)
         {
-            frameText[i].text = frames[i].ToString( );
+            frameText[i].text = frames[i].ToString( ); // Iterating through frame score boxes at the bottom, this assigns the ith saved frame int to the ith frame as per Unity Inspector
         }
     }
 
@@ -47,21 +41,21 @@ public class ScoreDisplay : MonoBehaviour {
             {  //display "-" in place of a "0"
                 output+="-";
             }
-            else if (miniFrame % 2 == 0 && (rolls[i - 1] + rolls[i] == 10)) // Spare 
+            else if ((miniFrame % 2 == 0 || miniFrame == 21)&& (rolls[i - 1] + rolls[i] == 10)) // Spare displays in the second bowl of a frame AND ALSO if a spare is detected in frame 21 (third, not caught by the even number checker)
             {
                 output += "/";
             }
-            else if (miniFrame >= 19 && rolls[i] == 10)
+            else if ((miniFrame >= 19 && rolls[i] == 10)) // Strike displays in the final frame as just X
             {
                 output += "X";
             }
-            else if (rolls[i] == 10)
+            else if (miniFrame % 2 == 1 && rolls[i] == 10) // Strike displays elsewhere with a space afterwards to ensure the correct character is assigned to the correct score box. Strikes cannot be scored in a second frame, they are classed as Spares
             {
                 output += "X ";
             }
             else
             {
-                output += rolls[i].ToString( );
+                output += rolls[i].ToString( ); // Handles normal bowl 
             }
         }
         return output;
